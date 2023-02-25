a99ec8a11c7dc32f9a5be39d6682e38895ae771c
Jason Zou <jasonzou@buffalo.edu>
2023-02-25 01:59:21 +0000

    test completed

--- src/main/java/CharComparison.java
+++ src/main/java/CharComparison.java
@@ -11,6 +11,11 @@ public class CharComparison {
      * @return -1 if A is less than B, 1 if A is greater than B, and 0 if the two arrays are identical.
      */
     public int compare(char[] a, char[] b){
-        return 0;
+        if (a.length > b.length) return compare(b, a) * -1;
+        for (int i = 0; i < a.length; i++){
+            if (a[i] < b[i]) return -1;
+            else if (a[i] > b[i]) return 1;
+        }
+        return a.length == b.length ? 0 : -1;
     }
 }
